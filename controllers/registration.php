db9924a8ceebaae12b4779a4f9d1cccc61b344aa
Park <parktesla@gmail.com>
2018-01-05 13:42:44 +0700

    asdasdasdasd

--- controllers/registration.php
+++ controllers/registration.php
@@ -1,6 +1,6 @@
 <?php
 
-class registration extends Controller {
+class Registration extends Controller {
 
     public function __construct() {
         parent::__construct();
@@ -22,16 +22,86 @@ class registration extends Controller {
     	}
     	$this->view->render($render);
     }
-    public function add(){
 
+    public function add(){
+      if( empty($this->me) || $this->format!='json' ) $this->error();
+
+        $this->view->setPage('path','Themes/manage/forms/registration');
+        $this->view->render('add');
     }
     public function edit($id=null){
+      $id = isset($_REQUEST["id"]) ? $_REQUEST["id"] : $id;
+        if( empty($id) || empty($this->me) || $this->format!='json' ) $this->error();
 
+        $item = $this->model->attend($id);
+        if( empty($item) ) $this->error();
+
+        $this->view->setData('item', $item);
+        $this->view->setPage('path','Themes/manage/forms/registration');
+        $this->view->render('add');
     }
     public function save(){
+      if( empty($_POST) ) $this->error();
 
+        $id = isset($_POST["id"]) ? $_POST["id"] : null;
+        if( !empty($id) ){
+            $item = $this->model->attend($id);
+            if( empty($item) ) $this->error();
+        }
+
+        try{
+            $form = new Form();
+            $form   ->post('attend_name')->val('is_empty');
+            $form->submit();
+            $postData = $form->fetch();
+
+            $has_name = true;
+            if( !empty($item) ){
+                if( $item['name'] == $postData['attend_name'] ) $has_name = false;
+            }
+            if( $this->model->is_attend($postData['attend_name']) && $has_name ){
+                $arr['error']['attend_name'] = 'This name have already !';
+            }
+
+            if( empty($arr['error']) ){
+                if(!empty($id)){
+                    $this->model->updateAttend($id, $postData);
+                }
+                else{
+                    $this->model->insertAttend($postData);
+                }
+
+                $arr['message'] = 'Saved !';
+                $arr['url'] = 'refresh';
+            }
+
+        } catch (Exception $e) {
+            $arr['error'] = $this->_getError($e->getMessage());
+        }
+        echo json_encode($arr);
     }
     public function del($id=null){
+        $id = isset($_REQUEST["id"]) ? $_REQUEST["id"] : $id;
+        if( empty($id) || empty($this->me) || $this->format!='json' ) $this->error();
 
+        $item = $this->model->attend($id);
+        if( empty($item) ) $this->error();
+
+        if( !empty($_POST) ){
+            if( !empty($item['permit']['del']) ){
+                $this->model->deleteAttend($id);
+                $arr['message'] = 'Deleted !';
+                $arr['url'] = 'refresh';
+            }
+            else{
+                $arr['message'] = 'Error : This can not delete by SYSTEM';
+            }
+            echo json_encode($arr);
+        }
+        else{
+            $this->view->setData('item', $item);
+            $this->view->setPage('path', 'Themes/manage/forms/registration');
+            $this->view->render('del');
+        }
     }
 }
