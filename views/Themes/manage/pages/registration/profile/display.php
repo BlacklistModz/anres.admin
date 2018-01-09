d98b47d71fc14f9b2850b354b9ad06b5f1a68989
Park <parktesla@gmail.com>
2018-01-09 15:55:00 +0700

    profile

--- views/Themes/manage/pages/registration/profile/display.php
+++ views/Themes/manage/pages/registration/profile/display.php
@@ -5,13 +5,29 @@
 		</div>
 		<div class="uiBoxWhite pam">
 			<ul>
-				<?php 
-				$a[] = array('key'=>'gender', 'label'=>'Genger', 'icon'=>'venus-mars');
-				$a[] = array('key'=>'email', 'label'=>'Email', 'icon'=>'mail');
+				<?php
+				// print_r($this->item);die;
+				$a[] = array('key'=>'title', 'label'=>'Name', 'icon'=>'id-card');
+				$a[] = array('key'=>'affiliation', 'label'=>'Affiliation', 'icon'=>'university');
+				$a[] = array('key'=>'region', 'label'=>'Region', 'icon'=>'flag');
+				$a[] = array('key'=>'address', 'label'=>'Address', 'icon'=>'home');
+				$a[] = array('key'=>'presentation_type', 'label'=>'Presentation type', 'icon'=>'desktop');
+				$a[] = array('key'=>'submission_type', 'label'=>'Submission type', 'icon'=>'file');
+				$a[] = array('key'=>'created', 'label'=>'Registered date', 'icon'=>'registered');
+				$a[] = array('key'=>'email', 'label'=>'E-mail', 'icon'=>'send');
 
 				foreach ($a as $key => $value) {
 					if( empty($this->item[$value['key']]) ) continue;
-
+					if ($value['key'] == 'title'){
+						$this->item[$value['key']] = $this->item[$value['key']]. '. '.$this->item['firstname']. ' '.$this->item['lastname'];
+					}
+					if ($value['key'] == 'address'){
+						$this->item[$value['key']] = $this->item[$value['key']].' '.$this->item['district']. ' '.$this->item['province']. ' '.$this->item['postal'];
+					}
+					if ($value['key'] == 'created'){
+						// $this->item[$value['key']] = $this->fn->q('time')->live($this->item[$value['key']]);
+						$this->item[$value['key']] = date("F j, Y (h:i:s A)", strtotime($this->item[$value['key']]));
+					}
 					echo '<li class="mt">
 								<i class="icon-'.$value['icon'].'"></i>
 								<span class="fwb">'.$value['label'].'</span> : '.$this->item[$value['key']].'
